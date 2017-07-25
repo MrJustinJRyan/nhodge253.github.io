ff6acfa3f3e3fc177522ecbec4a478469b336438
Nate Hodge <nhodge253@gmail.com>
2017-07-24 20:51:20 -0700

    Built undefined from commit b6823d7 on branch develop

---
+++ scripts/main.71eb8af9.js
@@ -0,0 +1,1 @@
+"use strict";$(document).ready(function(){$(".view-details").click(function(a){var b=a.target,c=$(b).parent().parent().children(".details");$(c).is(":visible")===!0?($(c).hide(),$(b).text("View details	»")):($(c).show(),$(b).text("Hide details »"))})});
